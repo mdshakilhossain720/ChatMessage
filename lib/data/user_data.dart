2203a4a4757fb538618795c02c4d7970a1a7a5df
MD shakil Hossain <mdshakilhossain720@gmail.com>
2025-06-25 15:15:00 +0600

    isue fix

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,14 +1,12 @@
-import 'package:chatapp/models/chat_user.dart';
-
 final String currentUserId = 'user_1';
 
-final List<ChatUser> users = [
-  ChatUser(
-    id: 'user_1',
-    name: 'John Doe',
+// final List<ChatUser> users = [
+//   ChatUser(
+//     id: 'user_1',
+//     name: 'John Doe',
 
-    avatarUrl: '',
-    isOnline: '',
-    isLastSeen: '',
-  ),
-];
+//     avatarUrl: '',
+//     isOnline: '',
+//     isLastSeen: '',
+//   ),
+// ];
